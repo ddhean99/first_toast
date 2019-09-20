edc7167a50929bf298bff8edb23597048fac0ab6
dheandefara <ddhean99@gmail.com>
2019-09-20 10:14:51 +0700

    commit1

---
+++ app/src/main/java/com/example/first_toast/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.first_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.first_toast.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
